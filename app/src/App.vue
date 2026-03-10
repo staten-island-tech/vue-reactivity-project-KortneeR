c01e5d7d0991f68d083439f7e9f7d1f9aea7f3c2
Kortnee Rong <kortneer@nycstudents.net>
2026-03-10 18:18:33 -0400

    hfdh

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,26 +5,6 @@
 </template>
 
 <script setup>
-const food = [
-  {
-    name: "vanilla-cone",
-    img:"/vanilla-cone.png" 
-  },
-  {
-    name: "choc-cone",
-    img:"/choc-cone.png"
-  },
-  {
-    name:"strawberry-icecream",
-    img:"/berry-ice.png"
-  },
-  {
-    name:"chocolate-icecream",
-    img:"/berry-ice.png"
-  }
-
-]
-
 
 
 </script>
